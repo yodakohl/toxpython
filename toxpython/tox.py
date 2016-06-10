445406caba2dcd3fea892912b6ba948d06850717
merlink <merlink@gmx.de>
2016-06-10 10:28:12 +0200

    fixed logging in get_friend_list

--- toxpython/tox.py
+++ toxpython/tox.py
@@ -359,7 +359,7 @@ class Tox():
         size = tox_self_get_friend_list_size(self._p)
         friendList = name_len_array = (c_uint32 * size)()
         tox_self_get_friend_list(self._p,friendList,None)
-        logger.info('Friend List: %s'%repr(friendList))
+        logger.info('Friend List: %s'%list(friendList))
         return friendList
 
     def friend_get_name_size(self,friendId):
